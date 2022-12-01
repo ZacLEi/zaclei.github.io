973f7fc5638f2efaa3775335def0b2541bc58bcd
liulei10 <liulei10@founder.com.cn>
2022-12-01 18:04:48 +0800

    init project

--- docs/js/FunnyTitle.js
+++ docs/js/FunnyTitle.js
@@ -3,11 +3,11 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
-        document.title = '我相信你还会回来的！';
+        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
         clearTimeout(titleTime);
     } else {
         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
-        document.title = '哈哈，我就知道！' + OriginTitle;
+        document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
