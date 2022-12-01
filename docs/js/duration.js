8dd65a30ab2c2d0dc2de03f1c7469cbcc624f4c4
liulei10 <liulei10@founder.com.cn>
2022-12-01 15:08:41 +0800

    Merge branch 'master' of github.com:ZacLEi/zaclei.github.io
    # Conflicts:
    #	public/2021/06/10/测试文章/index.html
    #	public/2022/11/22/hello-world/index.html
    #	public/404.html
    #	public/about/index.html
    #	public/archives/2021/06/index.html
    #	public/archives/2021/index.html
    #	public/archives/2022/11/index.html
    #	public/archives/2022/index.html
    #	public/archives/index.html
    #	public/categories/Java/index.html
    #	public/categories/index.html
    #	public/index.html
    #	public/js/duration.js
    #	public/links/index.html
    #	public/local-search.xml
    #	public/tags/index.html
    #	public/tags/原创/index.html

--- docs/js/duration.js
+++ docs/js/duration.js
@@ -22,7 +22,7 @@
         if(String(snum).length === 1 ){
             snum = "0" + snum;
         }
-        document.getElementById("timeDate").innerHTML = "本站已萌萌哒(✪ω✪) 运行&nbsp"+dnum+"&nbsp天";
+        document.getElementById("timeDate").innerHTML = "🚀 本站已正常运行&nbsp"+dnum+"&nbsp天";
         document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
     }
 
